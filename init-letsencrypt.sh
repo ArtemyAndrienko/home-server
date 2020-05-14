d9baa64f0a79e1f823de1192b0627f12b4339cfb
phil <phillip.ngan+manuka@gmail.com>
2020-05-14 22:39:44 +1200

    Working configuration for home server

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(nganfamily.com)
+domains=(nganfamily.com covid19.nganfamily.com ping.nganfamily.com ngan.familyds.com)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="phillipandkirsten@gmail.com" # Adding a valid address is strongly recommended
